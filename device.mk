0ab52e85803936afd7fdbd9eebbd4703bed4f90c
blinoff82 <blinov.in@gmail.com>
2018-07-08 09:05:43 +0300

    Remove Spectrum for EAS builds

--- device.mk
+++ device.mk
@@ -365,10 +365,6 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
-# Additional packages
-#PRODUCT_PACKAGES += \
-    Spectrum
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
